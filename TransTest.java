8896b0473febaa04ce8876cf581b675cda3056e0
Hitashee <sil.hitashee@gmail.com>
2016-06-28 09:17:43 +0530

    Update TransTest.java

--- TransTest.java
+++ TransTest.java
@@ -1,10 +1,6 @@
 package Assignment;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
@@ -30,29 +26,8 @@ public class TransTest {
 	
 	@Test
 	public void testBing() {
-		try{
-			
-			File f1=new File(Data.loc+Data.Bfile);
-		    File f2=new File(Data.loc+Data.Mfile);
-		    FileInputStream fi1=new FileInputStream(f1);
-		    FileInputStream fi2=new FileInputStream(f2); 
-		    DataInputStream di1=new DataInputStream(fi1);
-		    BufferedReader br1=new BufferedReader(new InputStreamReader(di1));
-		    DataInputStream di2=new DataInputStream(fi2);
-		    BufferedReader br2=new BufferedReader(new InputStreamReader(di2));
-		    String s1, s2;  
-		    while ((s1=br1.readLine())!=null && (s2=br2.readLine())!=null) 
-		     {
-		    	 Assert.assertEquals(s1,s2);
-		     }
-		    br1.close();
-		    br2.close();
-		} 
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	     
+		
+	     Assert.assertEquals(false, VerificationOfOP.verifyOutput());
 	   }
 	
 }
